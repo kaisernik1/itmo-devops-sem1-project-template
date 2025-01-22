67ad316d8dcc779bc9afe783bc5293c30e42722f
kaisernik1 <nikitakaiser@yandex.ru>
2025-01-22 18:47:04 +0300

    upd prepare

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Установка зависимостей
 go mod init myproject
 go get -u github.com/lib/pq
 
-# Подготовка базы данных
-psql -U validator -d project-sem-1 -c "CREATE TABLE IF NOT EXISTS prices (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL, create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
+export PGPASSWORD=val1dat0r
+
+echo "Waiting for PostgreSQL to become available..."
+until psql -h localhost -U validator -d project-sem-1 -c 'SELECT 1'; do
+  sleep 1
+done
+
+psql -h localhost -U validator -d project-sem-1 -c "CREATE TABLE IF NOT EXISTS prices (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL, create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
