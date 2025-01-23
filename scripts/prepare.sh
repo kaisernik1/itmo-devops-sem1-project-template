9d5bec41afaed8f95db18ccb9a41f195cd55f1c6
kaisernik1 <nikitakaiser@yandex.ru>
2025-01-23 13:49:57 +0300

    fix date

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -10,4 +10,4 @@ until psql -h localhost -U validator -d project-sem-1 -c 'SELECT 1'; do
   sleep 1
 done
 
-psql -h localhost -U validator -d project-sem-1 -c "CREATE TABLE IF NOT EXISTS prices (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL, create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
+psql -h localhost -U validator -d project-sem-1 -c "CREATE TABLE IF NOT EXISTS prices (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL, create_date DATE);"
